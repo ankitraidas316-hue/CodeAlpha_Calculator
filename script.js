9d7e1a22a6a370a0ab7f3b69d1820d939bc013c4
ankitraidas316-hue <ankitraidas316@gmail.com>
2025-08-20 23:01:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const display = document.getElementById("display");
+
+// Add value to display
+function appendValue(value) {
+    display.value += value;
+}
+
+// Clear the display
+function clearDisplay() {
+    display.value = "";
+}
+
+// Delete the last character
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Calculate result
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
